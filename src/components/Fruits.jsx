f191f95b1283cb89d1b4e2f78905ff9b5400a818
atiqul-islam <imonatikulislam@gmail.com>
2024-11-20 13:04:48 +0600

    add new fuits component

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,5 +1,13 @@
 export default function Fruits() {
   const fruits = ["Apple", "Mango", "Banana", "Orange", "Dragon"];
 
-  return <div>{fruits}</div>;
+  return (
+    <div>
+      <ul>
+        {fruits.map((fruit, i) => (
+          <li key={i}>{fruit}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
