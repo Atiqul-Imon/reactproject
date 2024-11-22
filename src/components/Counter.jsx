739fb284016907bc9b95077f652a1e42ee07fffc
atiqul-islam <imonatikulislam@gmail.com>
2024-11-22 10:11:41 +0600

    Update counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
+  function increment() {
+    return setCount(count + 1);
+  }
+
+  function reset() {
+    return setCount(0);
+  }
+
   return (
     <>
       <div>
         <p>{count}</p>
-        <button onClick={() => setCount(count + 1)}>Click here</button>
-        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={increment}>Click here</button>
+        <button onClick={reset}>Reset</button>
       </div>
     </>
   );
